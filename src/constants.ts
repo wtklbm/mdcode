5033a57db0cd84f3a49f9322382c1d5696583c7f
wtklbm <wtklbm@gmail.com>
2021-04-26 10:53:14 +0800

    🎈perf: 修改匹配路径正则

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,16 @@ export const codeTemplateStartReg = /^`{3} ?({[^}]+}|[-a-z]+)$/i;
 export const CODE_TAG = '```';
 
 /** 验证文档注释头的正则表达式 */
-export const commentTitleReg = /^(\/{2}[/!]? ?|# )/;
+export const commentTitleRegexps = [
+    // `/// ` || `//! ` || `// ` || `# `
+    /^(?:\/{2}[/!]? ?|# )(.+)$/,
+
+    // `/** xxx */`
+    /^\/\*{1,2}(.+)\*\/$/,
+
+    // `<!-- xxx -->`
+    /^<!--(.+)-->$/
+];
 
 export const pathReg = /^(?:([\\/]|\$[-a-z0-9_]+:)?[-a-z0-9]+|[.]{1,2}|[a-z]+:[\\/]?)(?:[/\\][-a-z0-9+&@#%?=~_|!:,;.]+)+([a-z0-9]|[\\/])/gi;
 
